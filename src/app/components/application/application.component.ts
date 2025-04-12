a8e4e871c274dac4d88934a73799e432f19138fb
AYARI Ibtihel <ibtihel.ayari@etudiant-fst.utm.tn>
2025-04-12 15:33:59 +0200

    commi application delete

--- src/app/components/application/application.component.ts
+++ src/app/components/application/application.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Application } from '../../models/application.model';
 import { ApplicationService } from '../../services/application.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-application',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './application.component.html',
   styleUrl: './application.component.css'
 })
 export class ApplicationComponent implements OnInit {
 applications: Application [] = []; // Store multiple applications
 selectedApplicationId: number | null = null;
+  selectedApplication: Application | null = null;
+  successMessage: string | null = null;
 
 constructor(private applicationService: ApplicationService){}
 ngOnInit(): void {
@@ -40,4 +43,30 @@ ngOnInit(): void {
     }
     
 
+     //deleteSelectedCandidat
+       selectApplication(application: Application) {
+          this.selectedApplication = { ...application };
+        }
+      
+        deleteSelectedApplication() {
+          if (this.selectedApplicationId !== null) {
+            const confirmation = window.confirm("Êtes-vous sûr de vouloir supprimer cette candidature ?");
+        
+            if (confirmation) {
+              this.applicationService.deleteApplication(this.selectedApplicationId).subscribe(() => {
+                this.successMessage = "Candidature supprimée avec succès !";
+                this.selectedApplicationId = null;
+                this.loadApplications();
+        
+                // Effacer le message après 3 secondes
+                setTimeout(() => {
+                  this.successMessage = null;
+                }, 3000);
+              });
+            }
+          } else {
+            alert('Veuillez sélectionner une candidature à supprimer.');
+          }
+        }
+        
 }
