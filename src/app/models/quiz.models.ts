bc5d7b3932cd5b2e87d0b0a6d0e294a1cede260e
AYARI Ibtihel <ibtihel.ayari@etudiant-fst.utm.tn>
2025-05-06 12:50:55 +0200

    commit quiz model

---
+++ src/app/models/quiz.models.ts
@@ -0,0 +1,50 @@
+import { Application } from "./application.model";
+
+export interface Quiz {
+    id: number;
+    applicationId: number;
+    application?: Application;
+    questions: Question[];
+    responses?: Response[];
+  }
+  
+  export interface Question {
+    id: number;
+    text: string;
+    correctAnswer: string;
+    moduleId?: number;
+    module?: Module | string;
+    quizId?: number;
+    quiz?: Quiz;
+    options: Option[];
+  }
+  
+  export interface Option {
+    id: number;
+    optionText: string;
+    questionId?: number;
+    question?: Question;
+  }
+  
+  export interface Module {
+    id: number;
+    name: string;
+    description: string;
+    questions?: Question[];
+  }
+  
+  export interface Response {
+    id: number;
+    answers: Answer[];
+    quizId: number;
+    quiz?: Quiz;
+    score: number;
+  }
+  
+  export interface Answer {
+    id: number;
+    answerText: string;
+    responseId: number;
+    response?: Response;
+  }
+  
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
+  private apiUrl = 'https://localhost:44353/api/quiz';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
 }
